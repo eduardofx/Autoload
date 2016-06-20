2563a1950c6d0dd524c5ad2d941ae2a9663ac862
Eduardo <eduardok.fx@gmail.com>
2016-06-20 15:18:16 -0300

    Add files via upload

---
+++ produto.php
@@ -0,0 +1,36 @@
+<?php
+
+class Produto
+{
+    private $Codigo;
+    private $Nome;
+    private $Salario;
+    //função para “setar” um valor no atributo salario
+    function SetSalario($Salario)
+    {
+        //antes de passar o valor do salario verificamos se o mesmo e um numero e positivo, se sim, passamos o valor para o atributo
+        if (is_numeric($Salario) and ($Salario > 0))
+        {
+            $this->Salario = $Salario;
+        }
+    }
+ 
+    function SetNome($Nome)
+    {
+        $this->Nome = $Nome;
+    }
+ 
+    function SetCodigo($Codigo)
+    {
+        $this->Codigo = $Codigo;
+    }
+ 
+    function Mostrar()
+    {
+    echo "O nome é: {$this->Nome}<br>";
+    echo "O salário é: R\$ {$this->Salario}<br>";
+    echo "O Código é: {$this->Codigo}";
+    }
+ 
+}
+?>
